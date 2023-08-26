69df44112de36706287bf419d1e153df60e89d1b
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2023-08-26 17:17:21 -0500

    car button

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link'
-import { Container } from '@/components/ui/container'
-import { MainNav } from './main-nav'
 
-const Navbar = () => {
+import { MainNav } from './main-nav'
+import { Container } from '@/components/ui/container'
+import getCategories from '@/lib/actions/get-categories'
+import NavbarActions from './navbar-actions'
+
+const Navbar = async () => {
+  const categories = await getCategories()
+
   return (
     <div className='border-b'>
       <Container>
@@ -10,7 +15,8 @@ const Navbar = () => {
           <Link href='/' className='ml-4 flex lg:ml-0 gap-x-2'>
             <p className='font-bold'>Store</p>
           </Link>
-          <MainNav data={[]} />
+          <MainNav data={categories} />
+          <NavbarActions />
         </div>
       </Container>
     </div>
